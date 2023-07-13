93d96bfb43b4278e892bca664a62b62f724112fd
chukwudi <omegapasky@gmail.com>
2023-07-13 14:42:30 -0700

    add processor

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Prints a string followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
